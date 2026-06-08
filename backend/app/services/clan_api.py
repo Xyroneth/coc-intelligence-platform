59635d2f464735d477fcebc5250216d421f6d799
Granth Agrawal <granth.agrawal2008@gmail.com>
2026-06-08 22:28:47 +0530

    V2 base stable player and clan search

--- backend/app/services/clan_api.py
+++ backend/app/services/clan_api.py
@@ -1,8 +1,5 @@
 import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
 
 API_TOKEN = os.getenv("CLASH_API_TOKEN")
 
@@ -10,7 +7,9 @@ HEADERS = {
     "Authorization": f"Bearer {API_TOKEN}"
 }
 
+
 def get_clan(clan_tag: str):
+
     clan_tag = "%23" + clan_tag.replace("#", "")
 
     url = f"https://api.clashofclans.com/v1/clans/{clan_tag}"
@@ -20,6 +19,16 @@ def get_clan(clan_tag: str):
         headers=HEADERS
     )
 
-    print("CLAN STATUS:", response.status_code)
+    data = response.json()
 
-    return response.json()
+    print("\n==================== CLAN DEBUG ====================")
+    print("STATUS:", response.status_code)
+    print("Clan Name:", data.get("name"))
+    print("Clan Tag:", data.get("tag"))
+    print("Clan Level:", data.get("clanLevel"))
+    print("Members:", data.get("members"))
+    print("War Wins:", data.get("warWins"))
+    print("Description:", data.get("description"))
+    print("====================================================\n")
+
+    return data
